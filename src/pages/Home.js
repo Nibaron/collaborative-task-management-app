2e6e442f5fbd1cc3fa0a60870c5d2d40157c26e4
Nibaron Kumar Das <36547410+Nibaron@users.noreply.github.com>
2023-09-07 14:16:59 +0600

    Final Initialization & Header added

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+
+
+export const Home = () => {
+
+    return (
+        <main>
+            <section className="max-w-7xl mx-auto py-7">
+                <div className="flex justify-start flex-wrap other:justify-evenly">
+
+                <p class="capitalize hover:uppercase">
+                    HomePage
+                </p>
+
+
+                </div>
+            </section>
+        </main>
+    );
+};
